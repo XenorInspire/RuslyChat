97a060a6a703d0d0e059ab91c5fb63df1f65b714
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-10-31 22:15:13 +0100

    ADD POC database connection with MySQL

--- ruslychat-server/src/main.rs
+++ ruslychat-server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use mysql::prelude::*;
+use mysql::*;
+
+fn main() -> Result<()> {
+
+    let url: &str = "mysql://test:test@localhost:3306/test";
+    let opts: Opts = Opts::from_url(url)?;
+
+    let pool: Pool = Pool::new(opts)?;
+    let mut conn: PooledConn = pool.get_conn()?;
+
+    Ok(())
 }
