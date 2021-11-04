9f2e308f3d42802b215ccf83c764a8bf975acd64
Antoine <fevre.pasquet@gmail.com>
2021-11-04 22:43:56 +0100

    First INSERT OK

--- ruslychat-server/src/main.rs
+++ ruslychat-server/src/main.rs
@@ -1,13 +1,34 @@
+extern crate mysql;
+
 use mysql::prelude::*;
 use mysql::*;
 
+struct User {
+    email: String,
+    username: String,
+    password: String,
+}
+
 fn main() -> Result<()> {
 
-    let url: &str = "mysql://test:test@localhost:3306/test";
+    let url: &str = "mysql://rust:rust@localhost:3306/ruslychat";
     let opts: Opts = Opts::from_url(url)?;
 
     let pool: Pool = Pool::new(opts)?;
     let mut conn: PooledConn = pool.get_conn()?;
+
+
+    let users = vec![
+        User { email: "coucou".to_string(), username: "toto".to_string(), password: "coucouToto".to_string() },
+    ];
+
+    conn.exec_batch(r"INSERT INTO user (email, username, password) VALUES (:email, :username, AES_ENCRYPT(:password, 'ruslychatAKATheBestChatEver'))",
+                    users.iter().map(|p| params! {
+                        "email" => p.email.clone(),
+                        "username" => p.username.clone(),
+                        "password" => p.password.clone(),
+                    })
+    )?;
 
     Ok(())
 }
