524cdcdff4efd07051226cc136dda35324288c47
Antoine <fevre.pasquet@gmail.com>
2021-11-14 19:46:56 +0100

    ADD log system

--- ruslychat-server/src/main.rs
+++ ruslychat-server/src/main.rs
@@ -1,4 +1,10 @@
+extern crate chrono;
+
 mod init;
+mod log;
+
+use log::Logger;
+use log::LogLevel;
 
 fn main() {
     let config = init::check_init_file();
@@ -7,4 +13,12 @@ fn main() {
     println!("{}", config.database);
     println!("{}", config.user);
     println!("{}", config.passwd);
+
+    let mut logger = Logger {
+        path: config.logs_directory,
+        log_file: "".to_string(),
+        max_size: 10
+    };
+
+    logger.log("coucou".to_string(), LogLevel::INFO);
 }
