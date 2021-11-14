642fff5f1274094817c17fd703925dc6a4e88e02
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-11-14 18:08:22 +0100

    ADD POC TCP stream with data exchange

--- ruslychat-server/src/main.rs
+++ ruslychat-server/src/main.rs
@@ -1,10 +1,48 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+
 mod init;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            // echo everything!
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
 
 fn main() {
     let config = init::check_init_file();
-    println!("{}", config.port);
-    println!("{}", config.logs_directory);
-    println!("{}", config.database);
-    println!("{}", config.user);
-    println!("{}", config.passwd);
+
+    let listener = TcpListener::bind(format!("{}:{}", "0.0.0.0", config.port.to_string())).unwrap();
+
+    // accept connections and process them, spawning a new thread for each one
+    println!("Server listening on port {}",config.port.to_string());
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    // connection succeeded
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+    // close the socket server
+    drop(listener);
+
 }
