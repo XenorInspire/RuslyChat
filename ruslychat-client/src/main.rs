e0fe7cdaa380dfc7ab2e63da8501f919446d67bd
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-11-20 23:33:35 +0100

    Update TCP functions + Clean code

--- ruslychat-client/src/main.rs
+++ ruslychat-client/src/main.rs
@@ -1,41 +1,9 @@
-use std::net::{TcpStream};
-use std::io::{Read, Write};
-use std::str::from_utf8;
-
 mod init;
+mod connect_tcp;
 
 fn main() {
+
     let config = init::check_init_file();
-    // match TcpStream::connect("localhost:3333") {
-
-    match TcpStream::connect(format!("{}:{}", config.domain, config.port_dest.to_string())) {
-        Ok(mut stream) => {
-            println!("Successfully connected to server in port {}", config.port_dest.to_string());
-
-            let msg = b"Hello!";
-
-            stream.write(msg).unwrap();
-            println!("Sent Hello, awaiting reply...");
-
-            let mut data = [0 as u8; 6]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is ok!");
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
-                    }
-                },
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
-                }
-            }
-        },
-        Err(e) => {
-            println!("Failed to connect: {}", e);
-        }
-    }
-    println!("Terminated.");
+    connect_tcp::start_connection(config);
 
 }
--- ruslychat-server/src/main.rs
+++ ruslychat-server/src/main.rs
@@ -1,48 +1,7 @@
-use std::thread;
-use std::net::{TcpListener, TcpStream, Shutdown};
-use std::io::{Read, Write};
-
 mod init;
-
-fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; 50]; // using 50 byte buffer
-    while match stream.read(&mut data) {
-        Ok(size) => {
-            // echo everything!
-            stream.write(&data[0..size]).unwrap();
-            true
-        },
-        Err(_) => {
-            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-            stream.shutdown(Shutdown::Both).unwrap();
-            false
-        }
-    } {}
-}
+mod tcp_listening;
 
 fn main() {
     let config = init::check_init_file();
-
-    let listener = TcpListener::bind(format!("{}:{}", "0.0.0.0", config.port.to_string())).unwrap();
-
-    // accept connections and process them, spawning a new thread for each one
-    println!("Server listening on port {}",config.port.to_string());
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move|| {
-                    // connection succeeded
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                /* connection failed */
-            }
-        }
-    }
-    // close the socket server
-    drop(listener);
-
+    tcp_listening::start_listening(config);
 }
