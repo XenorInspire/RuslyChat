c3c3d2349855bd5da727dc0cbb83a5e3c7384a48
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-11-21 17:12:38 +0100

    Clean code + ADD menu

--- ruslychat-client/src/main.rs
+++ ruslychat-client/src/main.rs
@@ -1,8 +1,21 @@
+use std::io;
+
 mod init;
 
 fn main() {
     let config = init::check_init_file();
-    println!("{}", config.domain);
-    println!("{}", config.port_dest);
-    println!("{}", config.logs_directory);
+    let mut answer = String::from("1");
+
+    while answer.eq("0") == false {
+        println!("========================\nWelcome to RuslyChat !\n========================");
+        println!("1 : Log in");
+        println!("2 : Manage settings");
+        println!("0 : Exit");
+
+        let mut buff = String::new();
+        io::stdin()
+            .read_line(&mut buff)
+            .expect("Reading from stdin failed");
+        answer = buff.trim().to_string();
+    }
 }
--- ruslychat-server/src/main.rs
+++ ruslychat-server/src/main.rs
@@ -2,9 +2,4 @@ mod init;
 
 fn main() {
     let config = init::check_init_file();
-    println!("{}", config.port);
-    println!("{}", config.logs_directory);
-    println!("{}", config.database);
-    println!("{}", config.user);
-    println!("{}", config.passwd);
 }
