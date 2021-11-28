232f23a189364bf15b37acd9a4f64f7d95042560
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-11-28 17:26:49 +0100

    ADD settings menu

--- ruslychat-client/src/init.rs
+++ ruslychat-client/src/init.rs
@@ -1,6 +1,8 @@
 extern crate ini;
 use ini::Ini;
+use regex::Regex;
 use std::fs;
+use std::io;
 use std::path::Path;
 use std::process;
 use std::{thread, time};
@@ -66,4 +68,103 @@ fn create_new_config_file() {
     conf.with_section(Some("LOGS SETTINGS"))
         .set("directory", "logs");
     conf.write_to_file(CONFIG_FILE).unwrap();
+}
+
+pub fn change_config_values(mut temp_config: Config) -> Config {
+    display_settings_menu(&temp_config);
+    let mut answer = String::from("1");
+
+    while answer.eq("0") == false {
+        display_settings_menu(&temp_config);
+
+        println!("Select setting to change value");
+        println!("1 : Domain (or IP address)");
+        println!("2 : Destination port");
+        println!("3 : Logs directory");
+        println!("0 : Exit settings");
+
+        let mut buff = String::new();
+        io::stdin()
+            .read_line(&mut buff)
+            .expect("Reading from stdin failed");
+        answer = buff.trim().to_string();
+
+        match &*answer {
+            "1" => temp_config.domain = change_domain(),
+            "2" => temp_config.port_dest = change_destination_port(),
+            "3" => temp_config.logs_directory = change_logs_directory(),
+            _ => (),
+        }
+    }
+
+    return temp_config;
+}
+
+fn display_settings_menu(config: &Config) {
+    println!("========================\n        Settings         \n========================");
+    println!("Domain => {}", config.domain);
+    println!("Destination port => {}", config.port_dest);
+    println!("Log directory => {}\n", config.logs_directory);
+}
+
+fn change_domain() -> String {
+    let ipv4_regex = Regex::new(r"(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}").unwrap();
+    let ipv6_regex = Regex::new(r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))").unwrap();
+    let domain_regex =
+        Regex::new(r"[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+").unwrap();
+    let mut buff = String::new();
+
+    while ipv4_regex.is_match(&*buff) == false
+        && ipv6_regex.is_match(&*buff) == false
+        && domain_regex.is_match(&*buff) == false
+    {
+        buff = String::from("");
+        println!("Enter a valid domain (or IP address)");
+        io::stdin()
+            .read_line(&mut buff)
+            .expect("Reading from stdin failed");
+        buff = buff.trim().to_string();
+    }
+
+    return buff;
+}
+
+fn change_destination_port() -> u16 {
+    let mut new_port: u16 = 0;
+
+    while new_port <= 1024 {
+        println!("Enter a valid destination port");
+        let mut buff = String::from("");
+
+        io::stdin()
+            .read_line(&mut buff)
+            .expect("Reading from stdin failed");
+        buff = buff.trim().to_string();
+
+        match buff.parse::<u16>() {
+            Err(e) => {
+                new_port = 0;
+                println!("Error: {}\n", e)
+            }
+            _ => new_port = buff.parse::<u16>().unwrap(),
+        }
+    }
+
+    return new_port;
+}
+
+fn change_logs_directory() -> String {
+    let mut buff = String::new();
+
+    while Path::new(&*buff).exists() == false {
+        println!("Enter a valid path for the log directory");
+
+        buff = String::from("");
+        io::stdin()
+            .read_line(&mut buff)
+            .expect("Reading from stdin failed");
+        buff = buff.trim().to_string();
+    }
+
+    return buff;
 }
--- ruslychat-client/src/main.rs
+++ ruslychat-client/src/main.rs
@@ -3,11 +3,11 @@ use std::io;
 mod init;
 
 fn main() {
-    let config = init::check_init_file();
+    let mut config = init::check_init_file();
     let mut answer = String::from("1");
 
     while answer.eq("0") == false {
-        println!("========================\nWelcome to RuslyChat !\n========================");
+        println!("========================\n Welcome to RuslyChat !\n========================");
         println!("1 : Log in");
         println!("2 : Manage settings");
         println!("0 : Exit");
@@ -17,5 +17,10 @@ fn main() {
             .read_line(&mut buff)
             .expect("Reading from stdin failed");
         answer = buff.trim().to_string();
+
+        match &*answer {
+            "2" => config = init::change_config_values(config),
+            _ => (),
+        }
     }
 }
